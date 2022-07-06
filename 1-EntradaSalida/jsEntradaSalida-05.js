a3fbfc1cd8093053895f7b50748f940fae49e320
hernan mielniczuk <mielniczuk.hd@gmail.com>
2022-07-06 14:16:47 -0300

    completado

--- 1-EntradaSalida/jsEntradaSalida-05.js
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -1,8 +1,18 @@
 /*
+Hernán Darío Mielniczuk
+DIV Y 
+Ej5 E/S
+
 Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	alert("ok");
+	let nombre
+	nombre = document.getElementById("txtIdNombre").value
+	let edad
+	edad = document.getElementById("txtIdEdad").value
+
+
+	alert("Su nombre es "+nombre+" y tiene "+edad+" años");
 }
 
