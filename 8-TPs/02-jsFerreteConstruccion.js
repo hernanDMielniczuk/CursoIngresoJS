27c62a86b91ee864e011ff0653bb545c7309011c
hernan mielniczuk <mielniczuk.hd@gmail.com>
2022-07-07 17:51:10 -0300

    realizado

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -3,6 +3,8 @@ Hernán Darío Mielniczuk
 DIV Y 
 TP2 E/S
 
+ENTREGADO
+
 2.	Para el departamento de Construcción:
 A.	 mostrar la cantidad de alambre a comprar  si se ingresara el largo y el ancho de un terreno rectangular y se debe alambra con tres hilos de alambre.
 B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
@@ -49,7 +51,7 @@ function Circulo ()
     radioTerreno = parseInt (radioTerreno);
 
     diametro = radioTerreno *2;
-    perimetro = diametro * 3.14;
+    perimetro = diametro * Math.PI;
 
     resultado = perimetro * 3;
 
